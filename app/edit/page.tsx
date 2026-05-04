6629aca259c86e1a95d75ae228d313cac4fcce4d
Jeroen Gilis <gilis@chalmers.se>
2026-05-04 10:45:48 +0200

    update edit/page.tsx for vercel v7

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,5 +1,7 @@
 "use client"; // Add this line at the top to make this a Client Component
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { supabase } from "../../src/lib/supabase";
