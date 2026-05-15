0290626c7d0ac12d07d5fe08e093c2ba52026198
Jeroen Gilis <gilis@chalmers.se>
2026-05-15 11:56:00 +0200

    report author ID

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,5 +1,17 @@
 import NotificationsClient from "./NotificationsClient";
+import { supabase } from "../../src/lib/supabase";
+import { useEffect } from "react";
 
 export default function NotificationsPage() {
+
+  useEffect(() => {
+    async function checkUser() {
+      const { data } = await supabase.auth.getUser();
+      console.log("USER:", data.user);
+    }
+  
+    checkUser();
+  }, []);
+
   return <NotificationsClient />;
 }
